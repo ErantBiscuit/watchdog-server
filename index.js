f11c971a577d414140afe34b9ad9b810aff15fc3
ErantBiscuit <dorian2.0@outlook.com>
2026-05-03 15:54:26 -0600

    Update version to 1.21.11 in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 // Variables de entorno (Se configuran en Render después)
 const HOST = process.env.SERVER_IP 
 const PORT = parseInt(process.env.SERVER_PORT) || 25565
-const VERSION = "1.20.1" // AJUSTA A TU VERSIÓN
+const VERSION = "1.21.11" // AJUSTA A TU VERSIÓN
 
 app.get('/', (req, res) => res.send('Watchdog Funcionando ✅'))
 app.listen(process.env.PORT || 3000)
